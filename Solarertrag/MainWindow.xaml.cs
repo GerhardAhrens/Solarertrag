00a809a1f1b360af9736287441bb19d15c0ca031
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2023-06-25 17:50:28 +0200

    Add Windows Design

--- Solarertrag/MainWindow.xaml.cs
+++ Solarertrag/MainWindow.xaml.cs
@@ -1,28 +1,71 @@
 ﻿namespace Solarertrag
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.ComponentModel;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
     using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
+    using System.Windows.Interop;
+    using System.Windows.Threading;
+
+    using EasyPrototypingNET.Core;
+    using EasyPrototypingNET.Core.SystemMetrics;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string DateFormat = "dd.MM.yy HH:mm";
+        private DispatcherTimer statusBarDate = null;
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+            WeakEventManager<Window, CancelEventArgs>.AddHandler(this, "Closing", this.OnClosing);
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "SizeChanged", this.OnSizeChanged);
+
+            try
+            {
+                this.InitTimer();
+                this.InfoDeviceType = SystemMetricsInfo.DetectingDeviceType();
+                this.statusbarUserDomainName.Content = UserInfo.TS().CurrentDomainUser;
+            }
+            catch (Exception ex)
+            {
+                string errorText = ex.Message;
+                throw;
+            }
+        }
+
+        private InfoDeviceType InfoDeviceType { get; set; }
+
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void OnSizeChanged(object sender, RoutedEventArgs e)
+        {
+            int countMonitors = SystemMetricsInfo.CountMonitors;
+
+            string windowSize = $"{this.ActualWidth.ToInt()}x{this.ActualHeight.ToInt()}x{countMonitors}:{this.InfoDeviceType}";
+            this.tbMonitorSize.Content = windowSize;
+        }
+
+        private void InitTimer()
+        {
+            this.statusBarDate = new DispatcherTimer();
+            this.statusBarDate.Interval = new TimeSpan(0, 0, 1);
+            this.statusBarDate.Start();
+            this.statusBarDate.Tick += new EventHandler(
+                delegate (object s, EventArgs a) {
+                    this.dtStatusBarDate.Content = DateTime.Now.ToString(DateFormat);
+                });
         }
     }
 }
