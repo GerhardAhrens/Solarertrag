d5468c3476c2f4b0c5e3811a16aab3f6a7b677a2
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2023-06-26 17:49:03 +0200

    *

---
+++ Solarertrag/Model/SolarertragMonat.cs
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------
+// <copyright file="SolarertragMonat.cs" company="Lifeprojects.de">
+//     Class: SolarertragMonat
+//     Copyright © Lifeprojects.de 2023
+// </copyright>
+//
+// <Framework>7.0</Framework>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>26.06.2023 17:48:14</date>
+//
+// <summary>
+// Klasse für 
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Solarertrag.Model
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SolarertragMonat
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SolarertragMonat"/> class.
+        /// </summary>
+        public SolarertragMonat()
+        {
+        }
+    }
+}
