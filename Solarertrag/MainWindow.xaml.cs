87a59ecdba3d2d76ff3601bc0282eb7e97d833cd
Gerhard Ahrens <gerhard.ahrens@pta.de>
2023-06-26 15:49:37 +0200

    *

--- Solarertrag/MainWindow.xaml.cs
+++ Solarertrag/MainWindow.xaml.cs
@@ -67,5 +67,29 @@
                     this.dtStatusBarDate.Content = DateTime.Now.ToString(DateFormat);
                 });
         }
+
+        private void MaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == System.Windows.WindowState.Normal)
+            {
+                window.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = System.Windows.WindowState.Normal;
+            }
+
+            window.Focus();
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            window.Focus();
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = System.Windows.WindowState.Minimized;
+        }
+
     }
 }
